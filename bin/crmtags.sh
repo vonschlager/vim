0183d67ab1ff4d297c54426860eb1713c69d012e
Jacek Lehmann <jacek.lehmann@more7.pl>
2015-04-25 11:47:11 +0200

    crm7 tags

---
+++ bin/crmtags.sh
@@ -0,0 +1,11 @@
+ctags-exuberant -f tags \
+-h \".php\" \
+-R \
+--exclude=\"\.git\" \
+--exclude=\*.js \
+--totals=yes \
+--tag-relative=yes \
+--PHP-kinds=+cf \
+--regex-PHP='/abstract class ([^ ]*)/\1/c/' \
+--regex-PHP='/interface ([^ ]*)/\1/c/' \
+--regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/'
